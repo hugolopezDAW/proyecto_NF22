aba46542bd2d39bf36b217b23fc378c7d025a849
jbladed <joanblade@elpuig.xeill.net>
2025-05-12 20:25:52 +0200

    Update controlador.java

--- controlador.java
+++ controlador.java
@@ -40,4 +40,6 @@ public class controlador {
     public List<contacto> buscarContactoPorEmail(String email) {
         return contacts.stream().filter(c -> c.getEmail().equalsIgnoreCase(email)).toList();
     }
+
+    //Comentario de pruebas
 }
