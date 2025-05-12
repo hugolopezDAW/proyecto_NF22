4791cdb18e921e8a62500f24308a1453fc4b3de1
Joan Bladé Delgado <jbladedelgado@gmail.com>
2025-05-12 20:29:06 +0200

    Quiero subir cosas!

--- controlador.java
+++ controlador.java
@@ -39,5 +39,7 @@ public class controlador {
     }
     public List<contacto> buscarContactoPorEmail(String email) {
         return contacts.stream().filter(c -> c.getEmail().equalsIgnoreCase(email)).toList();
+
+        //Comentario de Joan.
     }
 }
