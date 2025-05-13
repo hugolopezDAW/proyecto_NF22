90cde46ef72a8102f486485faf629f4c24d15d50
hugo <hugolopez@elpuig.xeill.net>
2025-05-13 19:23:48 +0200

    ampliación clase fileController

--- fileController.java
+++ fileController.java
@@ -4,5 +4,12 @@ public class fileController extends controlador {
     private File dataFolder;
     private File contactFolder;
 
+    @Override
+    public contacto nuevoContacto(String nombre, String apellido, String telefono, String email){
+        contacto c = super.nuevoContacto(nombre, apellido, telefono, email);
+
+        return c;
+    }
+
 
 }
